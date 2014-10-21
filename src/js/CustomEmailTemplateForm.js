d1934e80aeb21b31b5a04884d8f6f88282d96a47
unknown <kristoffersend@WMLI109232.groupinfra.com>
2014-10-21 02:12:21 +0200

    Attempted to set content from crm to editor on load, needs some more work

--- src/js/CustomEmailTemplateForm.js
+++ src/js/CustomEmailTemplateForm.js
@@ -1,3 +1,46 @@
-window.addEventListener('message', function(e){
-	Xrm.Page.getAttribute('cgi_template').setValue(e.data);
-});
+var cgi = cgi || {};
+
+cgi.customemailtemplates = cgi.customemailtemplates || {};
+
+cgi.customemailtemplates.formscript = {
+	onload: function(){
+		cgi.customemailtemplates.formscript.setupMessageListeners();
+		cgi.customemailtemplates.formscript.sendReadySignal();
+	},
+	sendReadySignal: function(){
+		var customHtml = Xrm.Page.ui.controls.get("WebResource_customemailtemplate_index"),
+		destination = customHtml.getObject().contentWindow,
+		messageToPost = {
+			messageType: 'xrmloaded'
+		};
+
+		destination.postMessage(JSON.stringify(messageToPost), '*');
+	},
+	setupMessageListeners: function(){
+		window.addEventListener('message', function(e){
+			var messageToPost,
+				data = JSON.parse(e.data);
+        
+        	if (!data || !data.messageType) {
+        		return;
+        	};
+
+        	switch(data.messageType){
+        		case 'requestContent':
+        			cgi.customemailtemplates.formscript.postContent(e.source);
+        		break;
+
+        		case 'content':
+        			Xrm.Page.getAttribute('cgi_template').setValue(data.content);
+        		break;
+        	}
+		});
+	},
+	postContent: function(target){
+		messageToPost = {
+			messageType: 'content',
+			content: Xrm.Page.getAttribute('cgi_template').getValue()
+		};
+		target.postMessage(JSON.stringify(messageToPost), '*');
+	}
+};
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,73 @@
+var cgi = cgi || {};
+
+cgi.customemailtemplates = cgi.customemailtemplates || {};
+
+cgi.customemailtemplates.customcontrol = (function(){
+	var editorLoaded,
+		xrmLoaded;
+	addEventHandlers();
+
+	return {
+		setEditorContent: function(content){
+			if (editorLoaded) {};
+			setContent(content);
+		},
+		requestContent: function (){
+	    	requestContent();
+	    },
+	    setEditorLoaded: function(loaded){
+	    	if(loaded || loaded === undefined){
+	    		editorLoaded = true;
+	    	}
+	    	if (loaded === false) {
+	    		editorLoaded = false;
+	    	};
+
+	    },
+	    isEditorLoaded: function(){return editorLoaded;},
+	    isXrmLoaded: function(){return xrmLoaded;}
+	}
+
+	function addEventHandlers(){
+		window.addEventListener('message', function(e){
+        	console.log(e.data);
+        	var data = JSON.parse(e.data),
+        		error;
+        	if (!data || !data.messageType) {
+        		return;
+        	};
+
+        	switch(data.messageType){
+        		case 'content':
+        			if (!editorLoaded) { return;};
+        			setContent(data.content);
+        			break;
+
+        		case 'xrmloaded':
+        			xrmLoaded = true;
+        			if (!editorLoaded) { return;};
+        			requestContent();
+    				break;
+        	}
+        });	  
+	}
+
+	function setContent(content){
+		if(!tinymce.activeEditor){
+			throw new Error('no active editor');
+		}
+
+		tinymce.activeEditor.setContent(content);
+	}
+
+	function requestContent(){
+		var message = {
+    		messageType: 'requestContent'
+    	};
+    	window.parent.postMessage(JSON.stringify(message), '*');
+	}
+})();
+
 window.onload = function(){
 	tinymce.init({
 		mode: "exact",
@@ -16,11 +86,20 @@ window.onload = function(){
     	plugins: "table,paste,image,textcolor",
     	paste_data_images: true,
     	setup: function(editor) {
+    		cgi.customemailtemplates.customcontrol.setEditorLoaded(true);
 	        editor.on('keyup', changeHandler);
 	        editor.on('change', changeHandler);
+	        if (!cgi.customemailtemplates.customcontrol.isXrmLoaded()) { return;};
+	        cgi.customemailtemplates.customcontrol.requestContent();
+
+
 	        function changeHandler(e) {
-	        	var message = editor.getContent();
-	            window.parent.postMessage(message, '*');
+	        	var messageToPost = {
+					messageType: 'content',
+					content: editor.getContent()
+				};
+
+	            window.parent.postMessage(JSON.stringify(messageToPost), '*');
 	        }
 	    }
 
